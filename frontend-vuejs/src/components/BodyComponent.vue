9a5fec37763197dfdb80bb8014a4ac29a87c0a81
Lucas Genari <oligena@hotmail.com>
2022-08-17 20:13:39 -0300

    added body component

--- frontend-vuejs/src/components/BodyComponent.vue
+++ frontend-vuejs/src/components/BodyComponent.vue
@@ -1,5 +1,11 @@
 <template>
-  Body
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img src="./../assets/book-and-glasses.png" alt="Book and glasses" class="img-fluid mt-5">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
