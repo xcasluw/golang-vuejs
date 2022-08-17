bf2019fa8e73d85729657da0c55c24a966c44b18
Lucas Genari <oligena@hotmail.com>
2022-08-17 20:16:19 -0300

    added footer component

--- frontend-vuejs/src/components/FooterComponent.vue
+++ frontend-vuejs/src/components/FooterComponent.vue
@@ -1,5 +1,7 @@
 <template>
-  Footer
+  <footer class="d-flex align-items-center justify-content-center mt-5">
+    Copyright &copy; {{ new Date().getFullYear() }}
+  </footer>
 </template>
 
 <script>
@@ -8,6 +10,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  footer {
+    background: black;
+    color: white;
+    min-height: 3em;
+    font-size: 0.8em;
+  }
 </style>
