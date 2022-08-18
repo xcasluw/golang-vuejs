8191f9da05a84fd433a43ffc0d851d7e1c0dc5bc
Lucas Genari <oligena@hotmail.com>
2022-08-18 08:16:05 -0300

    request for login api

--- frontend-vuejs/src/components/LoginComponent.vue
+++ frontend-vuejs/src/components/LoginComponent.vue
@@ -47,7 +47,25 @@ export default {
   },
   methods: {
     submitHandler() {
-      console.log('submit handler called - success')
+      const payload = {
+        username: this.email,
+        password: this.password
+      }
+
+      const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(payload)
+      }
+
+      fetch("http://localhost:8081/users/login", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.error) {
+            console.log("Error: ", data.message)
+          } else {
+            console.log(data)
+          }
+        })
     }
   }
 }
