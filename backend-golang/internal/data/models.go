e8c87946441de5674cb9f5aeea9da761f74be729
Lucas Genari <oligena@hotmail.com>
2022-08-19 14:40:12 -0300

    added token model

--- backend-golang/internal/data/models.go
+++ backend-golang/internal/data/models.go
@@ -3,11 +3,23 @@ package data
 import "time"
 
 type User struct {
-	Id        int       `json:"id"`
+	ID        int       `json:"id"`
 	Email     string    `json:"email"`
 	FirstName string    `json:"first_name,omitempty"`
 	LastName  string    `json:"last_name,omitempty"`
 	Password  string    `json:"password"`
+	Token     Token     `json:"token"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Token struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token"`
+	TokenHash []byte    `json:"-"`
+	Expiry    time.Time `json:"expiry"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
