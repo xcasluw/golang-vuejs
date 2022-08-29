03e870fd928b443d28dd4301a2c593857ed62731
Lucas Genari <oligena@hotmail.com>
2022-08-29 09:20:25 -0300

    feat: add list of users in the frontend

--- backend-golang/cmd/api/handlers.go
+++ backend-golang/cmd/api/handlers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"time"
+	"xcasluw/backend-golang/internal/data"
 )
 
 func (app *application) Login(w http.ResponseWriter, r *http.Request) {
@@ -91,4 +92,21 @@ func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
+	var users data.User
+	all, err := users.GetAll()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "success",
+		Data:    envelope{"users": all},
+	}
+
+	app.writeJSON(w, http.StatusOK, payload)
 }
--- frontend-vuejs/src/components/UsersComponent.vue
+++ frontend-vuejs/src/components/UsersComponent.vue
@@ -3,6 +3,24 @@
     <div class="row">
       <div class="col">
         <h1 class="mt-3">All Users</h1>
+        <hr>
+
+        <table class="table table-compact table-striped">
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in this.users" v-bind:key="user.id">
+              <td>
+                <router-link :to="`/admin/users/${user.id}`">{{user.last_name}}, {{user.first_name}}</router-link>
+              </td>
+              <td>{{user.email}}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -10,10 +28,35 @@
 
 <script>
 import Security from './security.js'
+import notie from 'notie'
 
 export default {
+  data() {
+    return {
+      users: [],
+
+    }
+  },
   beforeMount() {
     Security.requireToken()
+    fetch(`${process.env.VUE_APP_API_URL}/admin/users`, Security.requestOptions(''))
+      .then(response => response.json())
+      .then(response => {
+        if (response.error) {
+          notie.alert({
+            type: 'error',
+            text: response.message
+          })
+        } else {
+          this.users = response.data.users
+        }
+      })
+      .catch(error => {
+        notie.alert({
+          type: 'error',
+          text: error
+        })
+      })
   }
 }
 </script>
