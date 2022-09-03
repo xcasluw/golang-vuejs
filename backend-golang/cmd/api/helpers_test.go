a694c61a3586cb9e729c3ba84e2beefe17874196
Lucas <oligena@hotmail.com>
2022-09-02 23:28:25 -0300

    test: improved errorJSON test

--- backend-golang/cmd/api/helpers_test.go
+++ backend-golang/cmd/api/helpers_test.go
@@ -69,6 +69,8 @@ func Test_errorJSON(t *testing.T) {
 		t.Log(err)
 	}
 
+	testJSONPayload(t, rr)
+
 	errSlice := []string{
 		"(SQLSTATE 23505)",
 		"(SQLSTATE 22001)",
@@ -80,5 +82,19 @@ func Test_errorJSON(t *testing.T) {
 		if customErr != nil {
 			t.Error(customErr)
 		}
+		testJSONPayload(t, rr)
+	}
+}
+
+func testJSONPayload(t *testing.T, rr *httptest.ResponseRecorder) {
+	var requestPayload jsonResponse
+	decoder := json.NewDecoder(rr.Body)
+	err := decoder.Decode(&requestPayload)
+	if err != nil {
+		t.Error("received error when decoding errorJSON payload", err)
+	}
+
+	if !requestPayload.Error {
+		t.Error("error set to true in response from errorJSON, and it should be set to true")
 	}
 }
